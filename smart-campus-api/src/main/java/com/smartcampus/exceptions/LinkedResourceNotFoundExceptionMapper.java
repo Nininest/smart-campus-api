abd0e0d16afdb71b51560c48b288e7c1bb2ff330
Nininest <nisthake@gmail.com>
2026-04-27 14:01:16 +0545

    Linked Resource Exception Mapper completed

---
+++ smart-campus-api/src/main/java/com/smartcampus/exceptions/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,28 @@
+package com.smartcampus.exceptions;
+
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex){
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", 422);
+        body.put("error", "Unprocessable Entity");
+        body.put("message",
+                ex.getResourceType() + "with id " + ex.getResourceId() +
+                " referenced in the body doesn't exist. " +
+                "Ensure the " + ex.getResourceType().toLowerCase() + "Id field refers to an existing resource.");
+
+        return Response.status(422)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(body)
+                .build();
+    }
+}
